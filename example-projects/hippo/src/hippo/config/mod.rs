330565102135c3be5769c604fdbc91cd4a0667a0
zhouhai <zhouhai@huya.com>
2020-11-04 19:55:01 +0800

    add etc/test_kafka_pulsar_config.toml

--- example-projects/hippo/src/hippo/config/mod.rs
+++ example-projects/hippo/src/hippo/config/mod.rs
@@ -7,15 +7,62 @@ use std::sync::mpsc::Receiver;
 use std::sync::{Arc, Mutex};
 #[derive(Debug, Deserialize)]
 pub struct Config {
-    pub log: LogConfig,
+    pub global: GlobalOptions,
     pub source: Box<dyn SourceConfig>,
     pub sink: Box<dyn SinkConfig>,
+}
+
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct GlobalOptions {
+    pub log: LogConfig,
+    pub db: MysqlConfig,
+    pub kafka: KafkaConfig,
+    pub clickhouse: ClickhouseConfig,
+    pub pulsar: PulsarConfig,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct LogConfig {
     pub level: String,
 }
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct MysqlConfig {
+    pub addr: String,
+    pub idle: i32,
+    pub max: i32,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct KafkaConfig {
+    pub brokers: String,
+    pub groupId: String,
+    pub consumerOffset: String,
+    pub timeout: i32,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ClickhouseConfig {
+    pub host: String,
+    pub port: String,
+    pub username: String,
+    pub password: String,
+    pub dsnParams: String,
+    pub inputBufferSize: i32,
+    pub flushInterval: i32,
+    pub flushParallel: i32,
+    pub flushLimitMB: i32,
+    pub flushLimitSize: i32,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct PulsarConfig {
+    pub addr: String,
+    pub token: String,
+    pub timeout: i32,
+}
+
 #[typetag::serde(tag = "type")]
 pub trait SourceConfig: core::fmt::Debug {
     fn build(&self, tx: SyncSender<Vec<u8>>) -> Box<dyn Source>;
